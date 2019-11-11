a0186177e0002f0918b74e58663ffce8823f4752
ejgoodwin <ericagoodwin90@gmail.com>
2019-11-11 20:06:26 +1300

    new design

--- run.js
+++ run.js
@@ -12,4 +12,14 @@ $(document).ready(function() {
 		$('.website').css('display', 'block');
 	});
 
+	$('.js-hamburger').click(function() {
+		$(this).toggleClass('hamburger-cross');
+		$('.js-dropdown-menu').toggleClass('dropdown-menu--open');
+	});
+
+	$('.dropdown-menu__link').click(function() {
+		$('.js-dropdown-menu').removeClass('dropdown-menu--open');
+		$('.js-hamburger').removeClass('hamburger-cross');
+	})
+
 });
